093fb56b5993c099ad50af9751b1166e91830fe8
jmcamacho7 <camachosanchezjm@gmail.com>
2022-01-28 20:29:52 +0100

    Segundo paquete comenzado

--- src/test/java/PruebaV1.java
+++ src/test/java/PruebaV1.java
@@ -3,6 +3,8 @@ import modelos.Producto;
 public class PruebaV1 {
     public static void main(String[] args) {
        Producto objproducto = new Producto();
+        System.out.println(objproducto.toString());
+
 
 
     }
