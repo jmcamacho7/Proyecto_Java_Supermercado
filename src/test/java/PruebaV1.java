b921d4ee44d3953bbefef33150316f14f4e42372
jmcamacho7 <camachosanchezjm@gmail.com>
2022-02-01 18:52:36 +0100

    Java V1 acabado

--- src/test/java/PruebaV1.java
+++ src/test/java/PruebaV1.java
@@ -1,9 +1,19 @@
+import modelos.Almacen;
+import modelos.Cliente;
 import modelos.Producto;
+
+import java.sql.SQLOutput;
 
 public class PruebaV1 {
     public static void main(String[] args) {
        Producto objproducto = new Producto();
         System.out.println(objproducto.toString());
+
+        Almacen objalmacen = new Almacen();
+        System.out.println(objalmacen.toString());
+
+        Cliente objcliente = new Cliente();
+        System.out.println(objcliente.toString());
 
 
 
